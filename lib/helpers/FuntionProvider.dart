9f6918644fabfa9ebab6c4a6cf271997e7d92776
Abraham Hidy <ah224hb@student.lnu.se>
2020-08-12 09:13:16 +0200

    added shared preference getters

--- lib/helpers/FuntionProvider.dart
+++ lib/helpers/FuntionProvider.dart
@@ -5,6 +5,8 @@ class FuntionProvider {
   static String savedUsernameKey = 'USERNAMEKEY';
   static String savedEmailKey = 'EMAILKEY';
 
+  //Savers
+
   static Future<bool> saveLoggedIn(bool isLoggedIn) async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     return await preferences.setBool(savedLoggedInKey, isLoggedIn);
@@ -18,5 +20,22 @@ class FuntionProvider {
   static Future<bool> saveUsersEmail(String email) async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     return await preferences.setString(savedEmailKey, email);
+  }
+
+  //Retrievers
+
+  static Future<bool> getLoggedIn() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getBool(savedLoggedInKey);
+  }
+
+  static Future<String> getUsersName() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getString(savedLoggedInKey);
+  }
+
+  static Future<String> getUsersEmail() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getString(savedLoggedInKey);
   }
 }
